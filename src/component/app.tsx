8ff594d7834e97307c96bba081e9cc78e19732d8
Bartosz Liebek <bliebek@gmail.com>
2020-09-01 10:28:57 +0200

    feat(layout): implement basic app layout

--- src/component/app.tsx
+++ src/component/app.tsx
@@ -1,3 +1,7 @@
 import React from 'react';
+import './app.css';
 
-export default () => <div></div>;
+export default () => (<div className={'c-app'}>
+    <div className={'c-app__sidebar'}></div>
+    <div className={'c-app__content'}></div>
+</div>);
