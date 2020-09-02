ca811a92065427a93c3d4cf8819d6c14d128c8e7
Bartosz Liebek <bliebek@gmail.com>
2020-09-02 12:06:47 +0200

    refactor(api): use local file as datasource

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
 export default {
     data: {
-        // source: 'data/data.csv'
-        source: 'http://adverity-challenge.s3-website-eu-west-1.amazonaws.com/DAMKBAoDBwoDBAkOBAYFCw.csv'
+        source: '/data/data.csv'
+        // source: 'http://adverity-challenge.s3-website-eu-west-1.amazonaws.com/DAMKBAoDBwoDBAkOBAYFCw.csv'
+    },
+    chart: {
+        points: 100,
+        ticks: 5
     }
 };
--- src/service/api/api.ts
+++ src/service/api/api.ts
@@ -1,28 +1,30 @@
 import config from './../../config';
-import * as Papa from 'papaparse'
+import * as Papa from 'papaparse';
 
 export default async function getData() {
-  return new Promise((resolve, reject) => {
-    try {
-        console.log('AA', config.data.source);
-        Papa.parse(config.data.source, {
-            worker: true,
-            download: true,
-            header: true,
-            dynamicTyping: true,
-            delimiter: ',',
+    let data = await fetch(config.data.source)
+                        .then((response:any) => response.body.getReader().read())
+                        .then((result:any) => (new TextDecoder('utf-8')).decode(result.value));
 
-            complete:  (result:any) => {
-                console.log('parsing complete', result);
-                resolve(result.data);
-            },
-            error: (error:any, file:any) => {
-                console.warn('parsing error', error, file);
-                reject(error);
-            }
-        });
-    } catch (e) {
-      reject(e);
-    }
-  });
+    return new Promise((resolve, reject) => {
+        try {
+            Papa.parse(data, {
+                worker: true,
+                header: true,
+                dynamicTyping: true,
+                delimiter: ',',
+                skipEmptyLines: true,
+                complete:  (result:any) => {
+                    // console.log('parsing complete', result);
+                    resolve(result.data);
+                },
+                error: (error:any) => {
+                    // console.warn('parsing error', error);
+                    reject(error);
+                }
+            });
+        } catch (e) {
+          reject(e);
+        }
+    });
 }
